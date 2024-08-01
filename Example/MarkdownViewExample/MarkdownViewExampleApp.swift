c759663b94f1ef6fa7df8818abb66bec953d7eef
Andrew Zheng <aheze@getfind.app>
2024-08-01 13:52:46 -0700

    Add example

---
+++ Example/MarkdownViewExample/MarkdownViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownViewExampleApp.swift
+//  MarkdownViewExample
+//
+//  Created by Andrew Zheng on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
